e8a1c05dfd06b428f297001b072eb37f25e04d87
Joyce Costa <joycecosta90@gmail.com>
2020-12-03 18:04:39 -0300

    encontrando e resolvendo erros de sintaxe

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const routes = require('./routes');
 
 console.log(routes.someText);
 
-const server = http.createServer(routes.handler);
+cons server = http.createServer(routes.handler);
 
 server.listen(3000);
