43827384417c469cebd2460d3b4d6da240384b3a
Joyce Costa <joycecosta90@gmail.com>
2020-12-28 21:26:37 -0300

    extension

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+const express = require("express");
 
-const app = express();
+const productsController = require("./controllers/products");
 
-app.engine('handlebars', expressHbs());
-app.set('view engine', 'handlebars');
-app.set('views', 'views');
+const router = express.Router();
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+router.get("/", productsController.getProducts);
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public'))); 
-
-app.use('/admin', adminData.routes);
-app.use(shopRoutes);
-
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-});
-
-app.listen(3000);
+module.exports = router;
