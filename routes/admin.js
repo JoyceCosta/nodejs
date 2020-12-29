a9bfbd4d84ada21fbdcbc16f29fa00aaeaef93d0
Joyce Costa <joycecosta90@gmail.com>
2020-12-28 22:07:30 -0300

    products

--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,15 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-const products = [];
+// /admin/add-product => GET
+router.get("/add-product", productsController.getAddProduct);
 
-//admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-	res.render('add-product', { pageTitle: 'Add Product'});
-});
+// /admin/add-product => POST
+router.post("/add-product", productsController.postAddProduct);
 
-//admin/add-product =>POST
-router.post("/add-product", (req, res, next) => {
-	products.push({ title: req.body.title });
-	res.redirect("/");
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
