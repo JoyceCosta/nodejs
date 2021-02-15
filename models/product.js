da9219be9f1927127f591c6c05101b45d52d696e
Joyce Costa <joycecosta90@gmail.com>
2021-02-15 18:31:16 -0300

    extension

--- models/product.js
+++ models/product.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const p = path.join(
 	path.dirname(process.mainModule.filename),
@@ -7,31 +7,34 @@ const p = path.join(
 	"products.json"
 );
 
-const getProductsFromFile = cb => {
-  	fs.readFile(p, (err, fileContent) => {
+const getProductsFromFile = (cb) => {
+	fs.readFile(p, (err, fileContent) => {
 		if (err) {
-		cb([]);
+			cb([]);
 		} else {
-      cb(JSON.parse(fileContent));
-    }
+			cb(JSON.parse(fileContent));
+		}
 	});
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
-  }
+	constructor(title, imageUrl, description, price) {
+		this.title = title;
+		this.imageUrl = imageUrl;
+		this.description = description;
+		this.price = price;
+	}
 
-  save() {
-    getProductsFromFile(products => {
-      products.push(this);
+	save() {
+		getProductsFromFile((products) => {
+			products.push(this);
 			fs.writeFile(p, JSON.stringify(products), (err) => {
 				console.log(err);
 			});
-    });
-  }
+		});
+	}
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
-  }
+	static fetchAll(cb) {
+		getProductsFromFile(cb);
+	}
 };
