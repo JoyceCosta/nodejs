41ba99f88bddfbec8f648a0a28ccd00a3849276d
Joyce Costa <joycecosta90@gmail.com>
2021-04-06 21:24:05 -0300

    controllers and route

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,6 @@ exports.getAddProduct = (req, res, next) => {
 	res.render("admin/edit-product", {
 		pageTitle: "Add Product",
 		path: "/admin/add-product",
-		
 	});
 };
 
@@ -16,6 +15,18 @@ exports.postAddProduct = (req, res, next) => {
 	const product = new Product(title, imageUrl, description, price);
 	product.save();
 	res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+	res.render("admin/edit-product", {
+		pageTitle: "Edit Product",
+    path: "/admin/edit-product",
+    editing: editMode
+	});
 };
 
 exports.getProducts = (req, res, next) => {
