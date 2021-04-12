7cea2289c4b9b7d10c259ed4428f1202751b11f5
Joyce Costa <joycecosta90@gmail.com>
2021-04-11 21:47:22 -0300

    cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,12 +15,12 @@ exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
     res.render('shop/product-detail', {
-      product: product, 
-      pageTitle: product.title,
-      path: '/product'
+      product: product,
+      pageTitle: product.pageTitle,
+      path: '/products'
     });
   });
-}
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -39,23 +39,23 @@ exports.getCart = (req, res, next) => {
       for (product of products) {
         const cartProductData = cart.products.find(
           prod => prod.id === product.id
-          );
+        );
         if (cartProductData) {
-          cartProducts.push({productData: product, qty: cartProductData.qty});
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
         }
       }
-      res.render("shop/cart", {
-				path: "/cart",
-        pageTitle: "Your Cart",
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
         products: cartProducts
-			});
-		});
+      });
+    });
   });
 };
 
-exports.postCart =(req, res, next) => {
+exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
+  Product.findById(prodId, product => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect('/cart');
