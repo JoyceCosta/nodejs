a130465031921e2e4cd3f05acded2049747b0afa
Joyce Costa <joycecosta90@gmail.com>
2021-04-18 19:07:31 -0300

    fetching a single product with the where condition

--- models/product.js
+++ models/product.js
@@ -18,15 +18,13 @@ module.exports = class Product {
    );
   }
 
-  static deleteById(id) {
-   
-  }
+  static deleteById(id) {}
 
   static fetchAll() {
     return db.execute('SELECT * FROM products');
   }
 
   static findById(id) {
-
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
   }
 };
