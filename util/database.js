36300f1436ebf6dd183b2db1d7907d2b92c9c27d
Joyce Costa <joycecosta90@gmail.com>
2021-04-18 16:12:12 -0300

    database

--- util/database.js
+++ util/database.js
@@ -1,2 +1,10 @@
 const mysql = require('mysql2');
 
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'node-complete',
+  password: 'joyekim'
+});
+
+module.exports = pool.promise();
